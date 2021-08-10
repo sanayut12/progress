15976d3b6c1b24ff989184e41590527d3b311911
sanayut12 <sanayut12@gmail.com>
2021-08-10 18:51:39 +0700

    first commit

---
+++ lib/progress.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProgressBar extends StatefulWidget {
+  @override
+  _ProgressBarState createState() => _ProgressBarState();
+}
+
+class _ProgressBarState extends State<ProgressBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: double.infinity,
+      color: Colors.green,
+      child: Stack(
+        children: [
+          Container(
+            height: 100,
+            width: double.infinity,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  height: 7,
+                  width: double.infinity,
+                  color: Colors.blue,
+                )),
+                Expanded(
+                    child: Container(
+                  height: 7,
+                  width: double.infinity,
+                  color: Colors.amber,
+                ))
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            width: double.infinity,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 25,
+                  width: 25,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 3),
+                      color: Colors.white,
+                      shape: BoxShape.circle),
+                  child: Container(
+                    height: 14,
+                    width: 14,
+                    decoration: BoxDecoration(
+                        color: Colors.red, shape: BoxShape.circle),
+                  ),
+                ),
+                Container(
+                  height: 25,
+                  width: 25,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 3),
+                      color: Colors.white,
+                      shape: BoxShape.circle),
+                  child: Container(
+                    height: 14,
+                    width: 14,
+                    decoration: BoxDecoration(
+                        color: Colors.red, shape: BoxShape.circle),
+                  ),
+                ),
+                Container(
+                  height: 25,
+                  width: 25,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.red, width: 3),
+                      color: Colors.white,
+                      shape: BoxShape.circle),
+                  child: Container(
+                    height: 20,
+                    width: 20,
+                    decoration: BoxDecoration(
+                        color: Colors.red, shape: BoxShape.circle),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            width: double.infinity,
+            padding: EdgeInsets.only(bottom: 40, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("data"),
+                Text("data"),
+                Text("data"),
+              ],
+            ),
+            // child: ,
+          )
+        ],
+      ),
+    );
+  }
+}
